6bcff838bddd0de7e611d29dca7257dab05d5c6d
Kandrika Pritularga <k.pritularga@gmail.com>
2020-07-01 17:01:13 +0100

    initial commit

---
+++ R/demo_utils.R
@@ -0,0 +1,43 @@
+#' Calculate the logarithm of the odds of a probability
+#'
+#' @param p A probability (a values > 0 and less than 1)
+#' @return The logarithm (to base e) of the probability
+#' @examples
+#' logit(0.5)
+#' logit(0.75)
+#' logit(0.25)
+logit <- function(p) {
+  log(p/(1-p))
+}
+
+#' Calculate the inverse of the logit function
+#'
+#' @export
+#' @param logodds The logarithm (to base e) of some probabilities
+#' @return The probability that is the inverse of the log odds
+#' @examples
+#' ilogit(3)
+#' ilogit(-2)
+#' ilogit(0)
+
+ilogit <- function(logodds) {
+  1/(1 + exp(-logodds))
+}
+
+#' Plot a binomial likelihood function
+#'
+#' @export
+#' @param n Number of trials
+#' @param m Number of successes
+#' @return A ggplot of the likelihood function
+#' @examples
+#' likelihood(100, 60)
+#' likelihood(75, 25)
+#' likelihood(100, 33)
+#' @import ggplot2
+
+likelihood <- function(n = 100, m = 60) {
+  data_df <- dply::tibble(x = seq(0, 1,length.out = 1000),
+                          y = x^m * (1-x)^(n-m))
+  ggplot(data_df, aes(x = x, y = y)) + geom_line()
+}
---
+++ README.md
@@ -0,0 +1,38 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# demopkg
+
+<!-- badges: start -->
+
+<!-- badges: end -->
+
+The goal of `demopkg` is to provide some demonstrations of R functions
+and so vignettes.
+
+## Installation
+
+You can install the development version of demopkg from
+[GitHub](https://github.com/nikdraka/demopkg) with:
+
+``` r
+devtools::install_github("https://github.com/nikdraka/demopkg")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(demopkg)
+
+M <- make_data()
+```
+
+Use this code like this…
+
+``` r
+plot(rnorm(100))
+```
+
+<img src="man/figures/README-cars-1.png" width="100%" />
